96dab62194d6b8f240bc77197a631e4480fdaf19
Sean Sullivan <dev@seanmedia.ca>
2021-05-15 08:49:09 -0600

    Update docstrings for abstract endpoint module and class methods.

--- src/abstracts/endpoint.py
+++ src/abstracts/endpoint.py
@@ -1,38 +1,129 @@
 # -*- coding: utf-8 -*-
+"""Abstract endpoint class interface.
+
+This module defines an abstract endpoint class which provides an interface
+for subclasses to implement. Each of the abstract methods represents
+a standard HTTP request method.
+
+"""
 
 # Third-Party Imports
 import abc
+
+# Third-Party Imports
+from requests import Response
+
+# Local Imports
+from .api import AbstractAPI
 
 
 class AbstractEndpoint(abc.ABC):
     """
     Represents an abstract endpoint.
     """
+    api: AbstractAPI
+    path: str
 
     @abc.abstractmethod
-    def head(self, *args, **kwargs):
+    def head(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP HEAD request.
+
+        This method must call the `head` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/HEAD
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP GET request.
+
+        This method must call the `get` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def post(self, *args, **kwargs):
+    def post(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP POST request.
+
+        This method must call the `post` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def put(self, *args, **kwargs):
+    def put(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP PUT request.
+
+        This method must call the `put` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PUT
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def delete(self, *args, **kwargs):
+    def patch(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP PATCH request.
+
+        This method must call the `patch` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PATCH
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def options(self, *args, **kwargs):
+    def delete(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP DELETE request.
+
+        This method must call the `delete` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE
+
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def trace(self, *args, **kwargs):
+    def options(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP OPTIONS request.
+
+        This method must call the `options` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS
+
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def trace(self, *args, **kwargs) -> Response:
+        """
+        Abstract method for sending an HTTP TRACE request.
+
+        This method must call the `trace` method on the component API instance.
+
+        .. _MDN Web Docs:
+            https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/TRACE
+
+        """
         raise NotImplementedError
