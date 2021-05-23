a56bc94763815d8435cbe983962305a14e3eef6e
Sean Sullivan <dev@seanmedia.ca>
2021-05-23 11:33:27 -0600

    Make it easier to import classes from modules.

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,12 @@
 
 # pylint: skip-file
 
+from .base.adapters import TransportAdapter
+from .base.api import BasicAPI as API
+from .base.api import SessionAPI as Session
+from .base.endpoint import BasicEndpoint as Endpoint
+from .base.model import BasicModel as Model
+
 
 VERSION = (0, 0, 1, 'dev', 0)
 
--- src/decorators/__init__.py
+++ src/decorators/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # pylint: skip-file
+
+from .connection import confirm_connection
+from .json import json_response
+from .pagination import pagination
